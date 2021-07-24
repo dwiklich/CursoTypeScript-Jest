18d97a99fb181282d1d289e686779bb04960b1ea
dwiklich <d.wiklich@gmail.com>
2021-07-24 11:23:16 -0300

    Testando product.ts

---
+++ src/classes/product.spec.ts
@@ -0,0 +1,15 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number): Product => {
+  return new Product(name, price);
+};
+
+describe('Product', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should to have property "name" value "Caneta" and "price" value 4.99', () => {
+    const sut = createSut('Caneta', 4.99);
+    expect(sut).toHaveProperty('name', 'Caneta');
+    expect(sut.price).toBeCloseTo(4.99); // testando por ponto flutuante
+  });
+});
